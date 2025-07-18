9ce937c7d0238bcb883f50108d400bf6bd53bdb8
= <=>
2025-07-18 19:57:37 +0800

    Lab 8 Complete

---
+++ springboot-crashcourse/session2/lab8/myfirstApi/src/main/java/com/example/myfirstApi/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.example.myfirstApi.controller;
+
+import com.example.myfirstApi.model.Product;
+import com.example.myfirstApi.repository.ProductRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
